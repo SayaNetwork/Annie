c405d13aa403192fcf6f2d1af02b1702ed02eb1f
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-07-10 20:51:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram API credentials
-API_ID = os.getenv("30422005", None)
-API_HASH = os.getenv("5170ded206641d73215baf40175a6924", None)
-GPT_API = os.getenv("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")
+API_ID = os.getenv("", None)
+API_HASH = os.getenv("", None)
+GPT_API = os.getenv("")
 
 # Bot token and MongoDB URL fetched from environment variables
 BOT_TOKEN = os.getenv("BOT_TOKEN", None)
